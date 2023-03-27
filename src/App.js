85fc24b77c196e609c31704914e787a635cc6440
Anshul <anshulbhardwaj_cse19@its.edu.it>
2023-03-27 15:55:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+//components
+import Home from './Components/Home';
+import DataProvider from './context/DataProvider';
+
+function App() {
+  return (
+    <DataProvider>    
+      <Home />
+    </DataProvider>
+  );
+}
+
+export default App;
